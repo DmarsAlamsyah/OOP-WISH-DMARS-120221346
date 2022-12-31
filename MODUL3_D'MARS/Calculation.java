8e479beb83010564b9ce0da32583c6fcf1bec1de
DmarsAlamsyah <116094967+DmarsAlamsyah@users.noreply.github.com>
2022-12-31 13:42:49 +0700

    Add files via upload

---
+++ MODUL3_D'MARS/Calculation.java
@@ -0,0 +1,9 @@
+public class Calculation {
+        public static void main(String[] args) {
+            double radius, side, area;
+            double phi = 3.14;
+        }
+        public static setSquare(){
+            
+        }
+    }
